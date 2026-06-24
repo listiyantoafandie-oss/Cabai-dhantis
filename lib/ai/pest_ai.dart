2486bed9f5b477bbfde06d91a2eb254f929d8b37
listiyantoafandie-oss <listiyanto.afandie@gmail.com>
2026-06-24 08:59:19 +0700

    Update pest_ai.dart

--- lib/ai/pest_ai.dart
+++ lib/ai/pest_ai.dart
@@ -1,1 +1,18 @@
-
+class PestAI {
+  static String detect({
+    required bool daunKeriting,
+    required bool bintikHitam,
+    required bool daunKuning,
+  }) {
+    if (daunKeriting && daunKuning) {
+      return "TRIPS";
+    }
+    if (bintikHitam) {
+      return "JAMUR";
+    }
+    if (daunKuning) {
+      return "Defisiensi N";
+    }
+    return "Normal";
+  }
+}
