9e2526bafb64bf8bc17dcb2b0cc37ef2c3e18184
listiyantoafandie-oss <listiyanto.afandie@gmail.com>
2026-06-24 08:55:37 +0700

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+class CabaiApp extends StatelessWidget {
+  const CabaiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cabai AI Farm V3',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const HomeScreen(),
+    );
+  }
+}
