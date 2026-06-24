7e13b846787ba1723a06627c84bed05afe8b857e
listiyantoafandie-oss <listiyanto.afandie@gmail.com>
2026-06-24 09:00:51 +0700

    Update weather_service.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,1 +1,9 @@
-
+class WeatherService {
+  static Map<String, dynamic> getWeather() {
+    return {
+      "rain": true,
+      "temp": 25,
+      "risk": "Hujan tinggi - risiko jamur"
+    };
+  }
+}
