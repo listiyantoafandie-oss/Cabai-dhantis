5d363c7c92ed516532623497982268211a7fe25f
listiyantoafandie-oss <listiyanto.afandie@gmail.com>
2026-06-24 08:56:42 +0700

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'ai_screen.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Cabai AI Farm V3")),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Masuk AI Dashboard"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const AIScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
